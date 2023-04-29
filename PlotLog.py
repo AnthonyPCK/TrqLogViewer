73a558f9b192461ebdda48d440013b90546d53d2
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-04-29 20:50:56 +0200

    Update PlotLog.py

--- PlotLog.py
+++ PlotLog.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib as mpl
-import plotly.express as px
+import plotly.figure_factory as ff
 import pandas as pd
 
 
@@ -24,6 +24,6 @@ NbCol = len(A)
 #    #ax.set_xlabel("Z [N]")
 #    st.pyplot(f)
 
-fig = px.line(CSV, x="Index", y="Data", color='Chanel')
+fig = ff.line(CSV, x="Index", y="Data", color='Chanel')
 
 st.plotly_chart(fig, use_container_width=True)
