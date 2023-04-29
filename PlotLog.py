06bb9017f60e4e22d3fd303d5b673e78fc38688d
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-04-29 20:14:42 +0200

    Update PlotLog.py

--- PlotLog.py
+++ PlotLog.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 
+
+#mpl.rc('figure', max_open_warning = 0)
+
+CSV = pd.read_csv('trackLog-2021-oct.-28_13-55-24.csv',na_values="-")  
+
+A = CSV.columns
+NbCol = len(A)
+
+for i in A:
+    NomCol = i
+    ValCol = CSV.get(NomCol)
+    
+    f = mpl.pyplot.figure(figsize=(8,6))
+    ax = f.add_subplot(111)#    
+    ax.plot(ValCol, '-')
+    ax.grid()
+    ax.set_title(NomCol)
+    #ax.set_xlabel("Z [N]")
+    st.pyplot(f)
 
 
 
