265c9c0a5495509c1309bb5f9531beeed3edcf26
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-04-29 20:49:46 +0200

    Update PlotLog.py

--- PlotLog.py
+++ PlotLog.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib as mpl
+import plotly.express as px
 import pandas as pd
 
 
@@ -11,17 +12,18 @@ CSV = pd.read_csv('trackLog-2021-oct.-28_13-55-24.csv',na_values="-")
 A = CSV.columns
 NbCol = len(A)
 
-for i in A:
-    NomCol = i
-    ValCol = CSV.get(NomCol)
-    
-    f = mpl.pyplot.figure(figsize=(8,6))
-    ax = f.add_subplot(111)#    
-    ax.plot(ValCol, '-')
-    ax.grid()
-    ax.set_title(NomCol)
-    #ax.set_xlabel("Z [N]")
-    st.pyplot(f)
+#for i in A:
+#    NomCol = i
+#    ValCol = CSV.get(NomCol)
+#    
+#    f = mpl.pyplot.figure(figsize=(8,6))
+#    ax = f.add_subplot(111)#    
+#    ax.plot(ValCol, '-')
+#    ax.grid()
+#    ax.set_title(NomCol)
+#    #ax.set_xlabel("Z [N]")
+#    st.pyplot(f)
 
+fig = px.line(CSV, x="Index", y="Data", color='Chanel')
 
-
+st.plotly_chart(fig, use_container_width=True)
