17340a5a8e719a37bf1a5476faf373ee353f06ad
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-04-30 01:16:39 +0200

    Update PlotLog.py

--- PlotLog.py
+++ PlotLog.py
@@ -93,3 +93,56 @@ optionyh = st.selectbox(
     Ayh)
 figxHist = px.density_heatmap(CSV_num, x=optionxh, y=optionyh, nbinsx=100, nbinsy=100)
 st.plotly_chart(figxHist, use_container_width=True)
+
+'''
+## Action :
+'''
+import yfinance as yf
+NomAct1 = st.text_input('ID de l''action :', 'goog')
+
+def history(self, period="1mo", interval="1d",
+            start=None, end=None, prepost=False, actions=True,
+            auto_adjust=True, back_adjust=False,
+            proxy=None, rounding=False, tz=None, timeout=None, **kwargs):
+    """
+    :Parameters:
+        period : str
+            Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+            Either Use period parameter or use start and end
+        interval : str
+            Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+            Intraday data cannot extend last 60 days
+        start: str
+            Download start date string (YYYY-MM-DD) or _datetime.
+            Default is 1900-01-01
+        end: str
+            Download end date string (YYYY-MM-DD) or _datetime.
+            Default is now
+        prepost : bool
+            Include Pre and Post market data in results?
+            Default is False
+        auto_adjust: bool
+            Adjust all OHLC automatically? Default is True
+        back_adjust: bool
+            Back-adjusted data to mimic true historical prices
+        proxy: str
+            Optional. Proxy server URL scheme. Default is None
+        rounding: bool
+            Round values to 2 decimal places?
+            Optional. Default is False = precision suggested by Yahoo!
+        tz: str
+            Optional timezone locale for dates.
+            (default data is returned as non-localized dates)
+        timeout: None or float
+            If not None stops waiting for a response after given number of
+            seconds. (Can also be a fraction of a second e.g. 0.01)
+            Default is None.
+        **kwargs: dict
+            debug: bool
+                Optional. If passed as False, will suppress
+                error message printing to console.
+    """
+
+goog = yf.Ticker(NomAct1)
+data = goog.history()
+st.write(data.head())
