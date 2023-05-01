22eb0d4ec101a292e0cf2cf496e5ae3931650386
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-05-01 23:39:21 +0200

    Update PostDBfile.py

--- PostDBfile.py
+++ PostDBfile.py
@@ -84,16 +84,28 @@ df_Out = pd.DataFrame(columns = ['Distance',
                                  'CapaciteBatDecharge',
                                  'CapaciteBatCharge30',
                                  'CapaciteBatDecharge30',
+                                 'CapaciteBatCharge35',
+                                 'CapaciteBatDecharge35',
                                  'CapaciteBatCharge40',
                                  'CapaciteBatDecharge40',
+                                 'CapaciteBatCharge45',
+                                 'CapaciteBatDecharge45',
                                  'CapaciteBatCharge50',
                                  'CapaciteBatDecharge50',
+                                 'CapaciteBatCharge55',
+                                 'CapaciteBatDecharge55',
                                  'CapaciteBatCharge60',
                                  'CapaciteBatDecharge60',
+                                 'CapaciteBatCharge65',
+                                 'CapaciteBatDecharge65',
                                  'CapaciteBatCharge70',
                                  'CapaciteBatDecharge70',
+                                  'CapaciteBatCharge75',
+                                 'CapaciteBatDecharge75',
                                  'CapaciteBatCharge80',
                                  'CapaciteBatDecharge80',
+                                 'CapaciteBatCharge85',
+                                 'CapaciteBatDecharge85',
                                  'CapaciteBatCharge90',
                                  'CapaciteBatDecharge90'],
                                  dtype = 'float')
@@ -170,26 +182,50 @@ for ii in df_Trips.index:
         df_T["CapaBat30"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>25) & (df_T.NewSOC<35)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge30"] = df_T.CapaBat30/df_T.CapaBat30 * np.mean(df_T.CapaBat30[(df_T.diffNewSOC.copy()<0)].copy())
         df_T["CapaBatCharge30"] = df_T.CapaBat30/df_T.CapaBat30 * np.mean(df_T.CapaBat30[(df_T.diffNewSOC.copy()>0)].copy())
+        
+        df_T["CapaBat35"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>30) & (df_T.NewSOC<40)].copy()/df_T.diffNewSOC.copy()
+        df_T["CapaBatDecharge35"] = df_T.CapaBat35/df_T.CapaBat35 * np.mean(df_T.CapaBat35[(df_T.diffNewSOC.copy()<0)].copy())
+        df_T["CapaBatCharge35"] = df_T.CapaBat35/df_T.CapaBat35 * np.mean(df_T.CapaBat35[(df_T.diffNewSOC.copy()>0)].copy())
             
         df_T["CapaBat40"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>35) & (df_T.NewSOC<45)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge40"] = df_T.CapaBat40/df_T.CapaBat40 * np.mean(df_T.CapaBat40[(df_T.diffNewSOC.copy()<0)].copy())
         df_T["CapaBatCharge40"] = df_T.CapaBat40/df_T.CapaBat40 * np.mean(df_T.CapaBat40[(df_T.diffNewSOC.copy()>0)].copy())
         
+        df_T["CapaBat45"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>40) & (df_T.NewSOC<50)].copy()/df_T.diffNewSOC.copy()
+        df_T["CapaBatDecharge45"] = df_T.CapaBat45/df_T.CapaBat45 * np.mean(df_T.CapaBat45[(df_T.diffNewSOC.copy()<0)].copy())
+        df_T["CapaBatCharge45"] = df_T.CapaBat45/df_T.CapaBat45 * np.mean(df_T.CapaBat45[(df_T.diffNewSOC.copy()>0)].copy())
+        
         df_T["CapaBat50"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>45) & (df_T.NewSOC<55)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge50"] = df_T.CapaBat50/df_T.CapaBat50 * np.mean(df_T.CapaBat50[(df_T.diffNewSOC.copy()<0)].copy())
         df_T["CapaBatCharge50"] = df_T.CapaBat50/df_T.CapaBat50 * np.mean(df_T.CapaBat50[(df_T.diffNewSOC.copy()>0)].copy())
+        
+        df_T["CapaBat55"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>50) & (df_T.NewSOC<60)].copy()/df_T.diffNewSOC.copy()
+        df_T["CapaBatDecharge55"] = df_T.CapaBat55/df_T.CapaBat55 * np.mean(df_T.CapaBat55[(df_T.diffNewSOC.copy()<0)].copy())
+        df_T["CapaBatCharge55"] = df_T.CapaBat55/df_T.CapaBat55 * np.mean(df_T.CapaBat55[(df_T.diffNewSOC.copy()>0)].copy())
         
         df_T["CapaBat60"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>55) & (df_T.NewSOC<65)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge60"] = df_T.CapaBat60/df_T.CapaBat60 * np.mean(df_T.CapaBat60[(df_T.diffNewSOC.copy()<0)].copy())
         df_T["CapaBatCharge60"] = df_T.CapaBat60/df_T.CapaBat60 * np.mean(df_T.CapaBat60[(df_T.diffNewSOC.copy()>0)].copy())
         
+        df_T["CapaBat65"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>60) & (df_T.NewSOC<70)].copy()/df_T.diffNewSOC.copy()
+        df_T["CapaBatDecharge65"] = df_T.CapaBat65/df_T.CapaBat65 * np.mean(df_T.CapaBat65[(df_T.diffNewSOC.copy()<0)].copy())
+        df_T["CapaBatCharge65"] = df_T.CapaBat65/df_T.CapaBat65 * np.mean(df_T.CapaBat65[(df_T.diffNewSOC.copy()>0)].copy())
+        
         df_T["CapaBat70"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>65) & (df_T.NewSOC<75)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge70"] = df_T.CapaBat70/df_T.CapaBat70 * np.mean(df_T.CapaBat70[(df_T.diffNewSOC.copy()<0)].copy())
         df_T["CapaBatCharge70"] = df_T.CapaBat70/df_T.CapaBat70 * np.mean(df_T.CapaBat70[(df_T.diffNewSOC.copy()>0)].copy())
         
+        df_T["CapaBat75"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>70) & (df_T.NewSOC<80)].copy()/df_T.diffNewSOC.copy()
+        df_T["CapaBatDecharge75"] = df_T.CapaBat75/df_T.CapaBat75 * np.mean(df_T.CapaBat75[(df_T.diffNewSOC.copy()<0)].copy())
+        df_T["CapaBatCharge75"] = df_T.CapaBat75/df_T.CapaBat75 * np.mean(df_T.CapaBat75[(df_T.diffNewSOC.copy()>0)].copy())
+        
         df_T["CapaBat80"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>75) & (df_T.NewSOC<85)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge80"] = df_T.CapaBat80/df_T.CapaBat80 * np.mean(df_T.CapaBat80[(df_T.diffNewSOC.copy()<0)].copy())
         df_T["CapaBatCharge80"] = df_T.CapaBat80/df_T.CapaBat80 * np.mean(df_T.CapaBat80[(df_T.diffNewSOC.copy()>0)].copy())
+        
+        df_T["CapaBat85"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>80) & (df_T.NewSOC<90)].copy()/df_T.diffNewSOC.copy()
+        df_T["CapaBatDecharge85"] = df_T.CapaBat85/df_T.CapaBat85 * np.mean(df_T.CapaBat85[(df_T.diffNewSOC.copy()<0)].copy())
+        df_T["CapaBatCharge85"] = df_T.CapaBat85/df_T.CapaBat85 * np.mean(df_T.CapaBat85[(df_T.diffNewSOC.copy()>0)].copy())
         
         df_T["CapaBat90"] = -100*df_T.diffNewEnergy[(df_T.NewSOC>85) & (df_T.NewSOC<95)].copy()/df_T.diffNewSOC.copy()
         df_T["CapaBatDecharge90"] = df_T.CapaBat90/df_T.CapaBat90 * np.mean(df_T.CapaBat90[(df_T.diffNewSOC.copy()<0)].copy())
@@ -211,16 +247,28 @@ for ii in df_Trips.index:
                                 'CapaciteBatDecharge' : df_T.CapaBatDecharge.mean(),
                                 'CapaciteBatCharge30' : df_T.CapaBatCharge30.mean(),
                                 'CapaciteBatDecharge30' : df_T.CapaBatDecharge30.mean(),
+                                'CapaciteBatCharge35' : df_T.CapaBatCharge35.mean(),
+                                'CapaciteBatDecharge35' : df_T.CapaBatDecharge35.mean(),
                                 'CapaciteBatCharge40' : df_T.CapaBatCharge40.mean(),
                                 'CapaciteBatDecharge40' : df_T.CapaBatDecharge40.mean(),
+                                'CapaciteBatCharge45' : df_T.CapaBatCharge45.mean(),
+                                'CapaciteBatDecharge45' : df_T.CapaBatDecharge45.mean(),
                                 'CapaciteBatCharge50' : df_T.CapaBatCharge50.mean(),
                                 'CapaciteBatDecharge50' : df_T.CapaBatDecharge50.mean(),
+                                'CapaciteBatCharge55' : df_T.CapaBatCharge55.mean(),
+                                'CapaciteBatDecharge55' : df_T.CapaBatDecharge55.mean(),
                                 'CapaciteBatCharge60' : df_T.CapaBatCharge60.mean(),
                                 'CapaciteBatDecharge60' : df_T.CapaBatDecharge60.mean(),
+                                'CapaciteBatCharge65' : df_T.CapaBatCharge65.mean(),
+                                'CapaciteBatDecharge65' : df_T.CapaBatDecharge65.mean(),
                                 'CapaciteBatCharge70' : df_T.CapaBatCharge70.mean(),
                                 'CapaciteBatDecharge70' : df_T.CapaBatDecharge70.mean(),
+                                'CapaciteBatCharge75' : df_T.CapaBatCharge75.mean(),
+                                'CapaciteBatDecharge75' : df_T.CapaBatDecharge75.mean(),
                                 'CapaciteBatCharge80' : df_T.CapaBatCharge80.mean(),
                                 'CapaciteBatDecharge80' : df_T.CapaBatDecharge80.mean(),
+                                'CapaciteBatCharge85' : df_T.CapaBatCharge85.mean(),
+                                'CapaciteBatDecharge85' : df_T.CapaBatDecharge85.mean(),
                                 'CapaciteBatCharge90' : df_T.CapaBatCharge90.mean(),
                                 'CapaciteBatDecharge90' : df_T.CapaBatDecharge90.mean(),
                                 }])
@@ -241,13 +289,19 @@ df_Out = df_Out[(df_Out.Distance > 5)]
 fig100 = px.scatter(df_Out, x=df_Out.DateTrajet, y=df_Out.columns,title="Suivi du viellissement :")
 # plot(fig100)        
 
-SoC = np.arange(30.0, 90.1, 10)
+SoC = np.arange(30.0, 90.1, 5)
 CapaDech = np.array([df_Out.CapaciteBatDecharge30.mean(),
+                     df_Out.CapaciteBatDecharge35.mean(),
                      df_Out.CapaciteBatDecharge40.mean(),
+                     df_Out.CapaciteBatDecharge45.mean(),
                      df_Out.CapaciteBatDecharge50.mean(),
+                     df_Out.CapaciteBatDecharge55.mean(),
                      df_Out.CapaciteBatDecharge60.mean(),
+                     df_Out.CapaciteBatDecharge65.mean(),
                      df_Out.CapaciteBatDecharge70.mean(),
+                     df_Out.CapaciteBatDecharge75.mean(),
                      df_Out.CapaciteBatDecharge80.mean(),
+                     df_Out.CapaciteBatDecharge85.mean(),
                      df_Out.CapaciteBatDecharge90.mean()])
 
 fig101 = px.line(x=SoC, y=CapaDech,labels={
