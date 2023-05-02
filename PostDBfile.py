7ba3f2338d2b521d1aa732139f22339fb89ad982
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-05-03 00:27:07 +0200

    Update PostDBfile.py

--- PostDBfile.py
+++ PostDBfile.py
@@ -149,7 +149,7 @@ def posttreatmyvin(uploaded_file, df_FastLog, df_Trips, df_TripInfo, optionVIN):
             df_T["HV_A_corr"] = df_T.HV_A.copy()
             df_T.HV_A_corr[(df_T.HV_A < 0)] = df_T.HV_A_corr[(df_T.HV_A_corr < 0)] * Bat_Rend
             df_T["EnergyCor"] = np.cumsum(df_T.HV_A_corr.copy() * df_T.diffTime_S.copy() / 3600)
-            
+            df_T["SoCestim"] = df_T.SOC.iloc[0]
             
             
             fig1 = px.scatter(df_T, x=df_T.index, y=df_T.columns)
