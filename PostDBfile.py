5ceb04f82109cbecc9483bbadb202d529ab050de
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-05-03 15:27:20 +0200

    Update PostDBfile.py

--- PostDBfile.py
+++ PostDBfile.py
@@ -346,7 +346,7 @@ def posttreatmyvin(uploaded_file, df_FastLog, df_Trips, df_TripInfo, optionVIN):
     
     
     return df_Out, df
-
+    bar.empty()
 
 df_Out, df = posttreatmyvin(uploaded_file, df_FastLog, df_Trips, df_TripInfo, optionVIN)
 
