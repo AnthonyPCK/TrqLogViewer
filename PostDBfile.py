d0ad6504ada72f30ff1b33bacdf7abda6baa0840
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-05-03 23:30:01 +0200

    Update PostDBfile.py

--- PostDBfile.py
+++ PostDBfile.py
@@ -58,7 +58,9 @@ def loadsqlite(uploaded_file):
     df_TripInfo = pd.read_sql('SELECT VIN FROM TRIPINFO', conn)
     
     # On garde les point ou on a le signal de tension batterie HV
-    #df_FastLog = df_FastLog.loc[df_FastLog["HV_V"] > 1]
+    st.write(df_FastLog.head)
+    df_FastLog = df_FastLog.loc[df_FastLog["HV_V"] > 1]
+    st.write(df_FastLog.head)
     
     return df_FastLog, df_Trips, df_TripInfo
 
