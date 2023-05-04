780f602872dfe8eae79bf445d548eda1927af80a
AnthonyPCK <132145288+AnthonyPCK@users.noreply.github.com>
2023-05-04 12:07:13 +0200

    Update PostDbFile_dev.py

--- PostDbFile_dev.py
+++ PostDbFile_dev.py
@@ -8,20 +8,7 @@ Created on Sun Apr 30 11:05:36 2023
 
 #%reset -f
 #%clear
-
-import streamlit as st
-from sqlite3 import connect
-import pandas as pd
-import plotly.express as px
-import numpy as np
-from plotly.offline import plot
-import plotly.graph_objects as go
-import pathlib
-import uuid
-from scipy.optimize import least_squares
-
-w4 = st.button("Click me")
-st.write(w4)
-
-if w4:
-    st.write("Hello, Interactive Streamlit!")
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
